00ad027975049d3973bce13a99e6d98e98180c92
Syed Ali Asar <sa.asar14@gmail.com>
2024-09-21 12:34:13 -0400

    upda env vars

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from 'next/server';
 const ses = new SES({
   region: 'us-east-1',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY || '',
   },
 });
 const emailAddress = 'syedali.asar14@gmail.com';

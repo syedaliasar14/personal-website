0b6bdacf46af1e8b6d0c59a8a1b3b391909d253d
Syed Ali Asar <sa.asar14@gmail.com>
2025-04-18 16:23:55 -0400

    add buttons

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Skills from "./components/Skills";
+import Link from "next/link";
 
 export default function HomePage() {
   const name = 'Syed Ali Asar'
@@ -27,8 +28,16 @@ export default function HomePage() {
         <div className={`flex flex-col mt-8 md:mt-0 justify-center text-center md:text-left
           ${isVisible ? 'opacity-100' : 'opacity-0 -translate-x-20'} transition-opacity transition-transform duration-1000`}>
           <div className="flex text-4xl md:text-6xl font-bold justify-center md:justify-start">{name}</div>
-          <div className="flex text-xl pt-8 italic justify-center md:justify-start ">
+          <div className="flex pt-6 italic justify-center md:justify-start ">
             Here to help streamline, scale, and succeed with thoughtful and high-quality software solutions.
+          </div>
+          <div className="flex pt-6 font-medium justify-center md:justify-start items-center">
+            <Link href={'/projects'} className="border-2 rounded-full border-green-300 px-4 py-2 text-green-300 hover:bg-green-300 hover:text-slate-800 transition-colors duration-300">
+              View Portfolio
+            </Link>
+            <Link href={'/contact'} className="ml-4 border-2 border-green-300 bg-green-300 rounded-full px-4 py-2 text-slate-800 hover:bg-transparent hover:text-green-300 transition-colors duration-300">
+              Contact
+            </Link>
           </div>
         </div>
       </section>

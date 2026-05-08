158ce156f10fe1ea688e2575aebdcd9d53e8726e
Syed Ali Asar <sa.asar14@gmail.com>
2026-05-08 17:14:43 -0400

    update menu with pricing

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,5 +1,5 @@
 import { TextAlignJustify } from "lucide-react";
-import { SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription, Sheet } from "./ui/sheet";
+import { SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription, Sheet, SheetFooter } from "./ui/sheet";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -12,30 +12,43 @@ export default function Menu() {
       <SheetContent className="bg-foreground flex flex-col items-center">
         <SheetHeader>
           <SheetTitle></SheetTitle>
+          <SheetDescription></SheetDescription>
           <Image src="/devlabs-logo-full.svg" alt="Asar Dev Labs Logo" width={100} height={100} className="w-24" />
         </SheetHeader>
 
         {/* Navigation Links */}
-        <div className='px-4 mt-4 flex flex-col gap-4 uppercase tracking-[0.24em] text-background text-center whitespace-nowrap items-center'>
-          <Link href="/devlabs" className="block w-max mx-auto md:mx-0 hover:text-green-300 transition-colors">
+        <div className='px-4 mt-4 flex flex-col gap-4 uppercase tracking-[0.24em] text-background text-center font-medium whitespace-nowrap items-center'>
+          <Link href="/devlabs" className="block w-max mx-auto hover:text-green-300 transition-colors">
             Home
           </Link>
-          <Link href="/devlabs/business-websites" className="block w-max mx-auto md:mx-0 hover:text-green-300 transition-colors">
+          <Link href="/devlabs/business-websites" className="block w-max mx-auto hover:text-green-300 transition-colors">
             Business Websites
           </Link>
-          <Link href="/devlabs/startup-mvp" className="block w-max mx-auto md:mx-0 hover:text-green-300 transition-colors">
+          <Link href="/devlabs/business-websites#pricing" className="block text-xs -mt-3 w-max mx-auto hover:text-green-300 transition-colors">
+            • Pricing
+          </Link>
+          <Link href="/devlabs/startup-mvp" className="block w-max mx-auto hover:text-green-300 transition-colors">
             Startup MVPs
           </Link>
-          <Link href="/portfolio" className="block w-max mx-auto md:mx-0 hover:text-green-300 transition-colors">
+          <Link href="/devlabs/startup-mvp#pricing" className="block text-xs -mt-3 w-max mx-auto hover:text-green-300 transition-colors">
+            • Pricing
+          </Link>
+          <Link href="/portfolio" className="block w-max mx-auto hover:text-green-300 transition-colors">
             Portfolio
           </Link>
-          <Link href="/contact" className="block w-max mx-auto md:mx-0 hover:text-green-300 transition-colors">
+          <Link href="/contact" className="block w-max mx-auto hover:text-green-300 transition-colors">
             Contact
           </Link>
-          <Link href="/" className="block w-max mx-auto md:mx-0 hover:text-green-300 transition-colors">
+          <Link href="/" className="block w-max mx-auto hover:text-green-300 transition-colors">
             About
           </Link>
         </div>
+
+        <SheetFooter>
+          <p className="text-xs text-balance text-center mt-8 text-muted-foreground">
+            © {new Date().getFullYear()} Asar Dev Labs. All rights reserved.
+          </p>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
